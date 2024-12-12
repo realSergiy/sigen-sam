498284a0f5cb353c2099d7a3c3a26ff5234c52bc
realSergiy <sergiy.yeskov@gmail.com>
2024-12-12 11:03:34 +1100

    feat: two backends

--- frontend/src/demo/DemoConfig.tsx
+++ frontend/src/demo/DemoConfig.tsx
@@ -24,15 +24,14 @@ export const DEMO_SHORT_NAME = 'SAM 2 Demo';
 export const RESEARCH_BY_META_AI = 'By Meta FAIR';
 export const DEMO_FRIENDLY_NAME = 'Segment Anything 2 Demo';
 export const VIDEO_WATERMARK_TEXT = `Modified with ${DEMO_FRIENDLY_NAME}`;
-export const PROJECT_GITHUB_URL =
-  'https://github.com/facebookresearch/sam2';
+export const PROJECT_GITHUB_URL = 'https://github.com/facebookresearch/sam2';
 export const AIDEMOS_URL = 'https://aidemos.meta.com';
 export const ABOUT_URL = 'https://ai.meta.com/sam2';
 export const EMAIL_ADDRESS = 'segment-anything@meta.com';
 export const BLOG_URL = 'http://ai.meta.com/blog/sam2';
 
-export const VIDEO_API_ENDPOINT = 'http://localhost:7263';
-export const INFERENCE_API_ENDPOINT = 'http://localhost:7263';
+export const VIDEO_API_ENDPOINT = 'http://localhost:7264';
+export const INFERENCE_API_ENDPOINT = 'http://localhost:7265';
 
 export const demoObjectLimit = 3;
 
